6a8fdea4c2d0110510446b7758718475d88b1270
godswill james <unekwuojoabah4@gmail.com>
2026-01-05 21:43:41 +0100

    made few fixes..

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ fn main() {
     // Tuple is a  compound data type in Rust  It's used to store heterogenouse sequence of elements. 
     // That's value of differnt data types can be stored.
     let tuple1:(&str,i32,f64) = ("Hello",  23, 20.4);
-    println("Tuple is {:?}", tuple1);
-    println("First value for the tuple is ", tuple1.0);
+    println!("Tuple is {:?}", tuple1);
+    println!("First value for the tuple is {}", tuple1.0);
 }
