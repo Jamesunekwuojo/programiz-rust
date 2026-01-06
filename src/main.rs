c09a0d7e4ff5496a206fdb2dded1335f7a6dd39d
godswill james <unekwuojoabah4@gmail.com>
2026-01-06 23:39:18 +0100

    adding reading docs..

--- src/main.rs
+++ src/main.rs
@@ -12,4 +12,13 @@ fn main() {
     let tuple1:(&str,i32,f64) = ("Hello",  23, 20.4);
     println!("Tuple is {:?}", tuple1);
     println!("First value for the tuple is {}", tuple1.0);
+
+    // Exploring ownership concept..
+    The major aim of the ownership is to manage heap.
+    Stack and Heap are both data structures in Rust.
+    The Stack Data sctrucures operate LIFO ---> that's Last I First Out. Think of it as 
+    arranging plates on stacking plates on each other.
+    Adding data on stack is know as  --> pushing
+    Removing data from the stack is known as  -- Pulling
+    
 }
